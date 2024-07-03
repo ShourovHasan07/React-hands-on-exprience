3911de158e8f7e27c8c7ae2d8869d537f9119638
shourovhasan~ <shourovhasan001@gmail@.com>
2024-07-04 02:19:24 +0600

    set use state

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useCallback,useEffect,useRef} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,19 +9,51 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState('')
 
+  const passwordRef = useRef(null)
+
+  const generatePassword =useCallback(()=>{
+    let pass =""
+    let str = 
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str +="0123456789"
+    if(charAllowed) str+="!@#$%^&*()_+"
+
+    for(let i=1; i<length;i++){
+    const char = Math.floor( Math.random()*str.length+1)
+    pass+=str.charAt(char)
+
+    }
+setPassword(pass)
+
+  },[length,numberAllowed,charAllowed])
+
+  const copyPasswordToClipboard =()=>{
+    window.navigator.clipboard.writeText(password)
+    passwordRef.current.select()
+   
+  }
+
+  useEffect(()=>{
+generatePassword()
+  },[length,numberAllowed,charAllowed])
+
+
   return (
     <div className="bg-gray-100 flex items-center justify-center h-screen">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-xs">
+      <div className="bg-white p-9 rounded shadow-md w-full max-w-xs">
         <h2 className="text-2xl font-bold mb-4 text-emerald-600">Password Generator</h2>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
           <input 
             type="text" 
             value={password}
             className='outline-none w-full py-1 px-3'
+
             placeholder='password'
             readOnly
+            ref={ passwordRef}
           />
           <button 
+          onClick={copyPasswordToClipboard}
             className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
           >
             Copy
@@ -36,13 +68,35 @@ function App() {
               max={100}
               value={length}
               onChange={(e) => setLength(e.target.value)}
-             
+              className="cursor-pointer"
+              id=''
+              name=''
             />
-            <div className='text-center mt-2'>
-              Length: {length}
-            </div>
+           <label htmlFor="length">Length:{length}</label>
             
           </div>
+          <input
+           type="checkbox"
+           defaultChecked={numberAllowed}
+           onChange={()=>{
+            setNumberAllowed((prev)=>!prev)
+           }}
+           
+            name="" 
+            id="" />
+            <label htmlFor="number">Numbers</label>
+
+
+            <input
+           type="checkbox"
+           defaultChecked={charAllowed}
+           onChange={()=>{
+            setNumberAllowed((prev)=>!prev)
+           }}
+           
+            name="" 
+            id="" />
+            <label htmlFor="charInput">Character </label>
         </div>
       </div>
     </div>
